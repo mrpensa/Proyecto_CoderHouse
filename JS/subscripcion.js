089bcdeb354f60f16e0568b0775acb4ad0acb425
mr_pensa <mariopensa97@gmail.com>
2022-03-02 22:17:00 -0300

    API

---
+++ JS/subscripcion.js
@@ -0,0 +1,18 @@
+let btn = document.getElementById("btnSub");
+let email = document.getElementById("emailSub").value;
+
+function nuevoSub(){
+
+    
+    btn.value = 'Enviando...';
+
+    setTimeout(() => {
+        btn.value = 'Registro guardado';
+        setTimeout(() => {
+            location.reload();
+        }, 1500);
+    }, 3000);
+
+    
+   
+}
