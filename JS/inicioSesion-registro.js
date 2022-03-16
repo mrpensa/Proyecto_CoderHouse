9efd470a8dec49ef2e5732384d10eca4223252b3
mr_pensa <mariopensa97@gmail.com>
2022-03-16 20:17:33 -0300

    cambio de calendarios

--- JS/inicioSesion-registro.js
+++ JS/inicioSesion-registro.js
@@ -97,10 +97,3 @@ function validar(){
     }
 
 }
-
-fetch('https://jsonplaceholder.typicode.com/posts')
-    .then( (resp) => resp.json() )
-    .then( (data) => {
-        console.log( data[0].title )
-        console.log( data[0].body )
-    })
